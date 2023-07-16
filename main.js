11fc00431574b4d345993463b14c04b89ce4171f
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-07-16 16:57:23 +0200

    Create class for node (which is a position of knight along with posibility of arrray of nodes with posibble moves), and a class of tree, that contains root node and another levels, now i need to add method that create another levels

--- main.js
+++ main.js
@@ -1,12 +1,58 @@
+function getOutOfBoard(array) {
+  function checkOnBoard(number) {
+    if (number > 0 && number <= 8) {
+      return true;
+    } else return false;
+  }
+  if (checkOnBoard(array[0]) && checkOnBoard(array[1])) return true;
+  else return false;
+}
+
 function getAllMoves(position) {
-  const case1 = [position[0] + 1, position[1] - 2];
-  const case2 = [position[0] + 2, position[1] - 1];
-  const case3 = [position[0] + 2, position[1] + 1];
-  const case4 = [position[0] + 1, position[1] + 2];
-  const case5 = [position[0] - 1, position[1] + 2];
-  const case6 = [position[0] - 2, position[1] + 1];
-  const case7 = [position[0] - 2, position[1] - 1];
-  const case8 = [position[0] - 1, position[1] - 2];
+  const movesArray = [
+    [position[0] + 1, position[1] - 2],
+    [position[0] + 2, position[1] - 1],
+    [position[0] + 2, position[1] + 1],
+    [position[0] + 1, position[1] + 2],
+    [position[0] - 1, position[1] + 2],
+    [position[0] - 2, position[1] + 1],
+    [position[0] - 2, position[1] - 1],
+    [position[0] - 1, position[1] - 2],
+  ];
+  const movesSorted = movesArray.filter((e) => {
+    return getOutOfBoard(e);
+  });
+  return movesSorted;
+}
+
+class Node {
+  constructor(field) {
+    this.field = field;
+    this.moves = null;
+  }
+}
+
+class Tree {
+  constructor(node) {
+    this.root = node;
+  }
+
+  getAnotherMove(node) {
+    const moves = getAllMoves(node.field);
+    const movesNodes = [];
+    for (let move of moves) {
+        const node = new Node(move);
+        movesNodes.push(node);
+    };
+    node.moves = movesNodes;
+  }
+
 }
 
 function knightMoves(a, b) {}
+
+const rootNode = new Node([2, 2]);
+const testTree = new Tree(rootNode);
+testTree.getAnotherMove(testTree.root);
+console.log(testTree);
+
