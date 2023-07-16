78d2b47a79ed4f40ae07ad83e46681e795400dc3
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-07-16 14:46:57 +0200

    First commit - create function that counts every possible move, out of initial location

---
+++ main.js
@@ -0,0 +1,12 @@
+function getAllMoves(position) {
+  const case1 = [position[0] + 1, position[1] - 2];
+  const case2 = [position[0] + 2, position[1] - 1];
+  const case3 = [position[0] + 2, position[1] + 1];
+  const case4 = [position[0] + 1, position[1] + 2];
+  const case5 = [position[0] - 1, position[1] + 2];
+  const case6 = [position[0] - 2, position[1] + 1];
+  const case7 = [position[0] - 2, position[1] - 1];
+  const case8 = [position[0] - 1, position[1] - 2];
+}
+
+function knightMoves(a, b) {}
